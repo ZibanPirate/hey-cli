1e3b1e66d41299b3af837a94baad0c988a09c5c7
Zakaria Mansouri <zakman.dev@gmail.com>
2024-12-23 16:39:13 +0100

    rename package from "cli" to "hey" and update dependencies; implement version printing in CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+/// Ask your CLI, next command will be auto-generated.
+#[derive(Parser, Debug)]
+#[command( about, long_about = None)]
+struct Args {
+    /// Print version information
+    #[arg(short, long)]
+    version: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.version {
+        const VERSION: &str = env!("CARGO_PKG_VERSION");
+        println!("Version: {}", VERSION);
+    }
 }
