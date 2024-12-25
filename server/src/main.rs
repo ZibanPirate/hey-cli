73a7feea28b28abe7b9ff17fcb1d99605946b7de
Zakaria Mansouri <zakman.dev@gmail.com>
2024-12-25 02:57:03 +0100

    implement initial server setup with Axum and CLI prompt endpoint

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{extract::Query, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // converts tracing records to stdout logs
+    #[cfg(debug_assertions)] // debug mode
+    let max_log_level = tracing::Level::TRACE;
+    #[cfg(not(debug_assertions))] // release mode
+    let max_log_level = tracing::Level::WARN;
+    tracing_subscriber::fmt()
+        .with_max_level(max_log_level)
+        .init();
+
+    let app = Router::new().route("/cli-prompt", get(get_cli_prompt));
+
+    let fallback_port = "3000";
+    let port = std::env::var("PORT").unwrap_or(fallback_port.to_string());
+    let bind = format!("0.0.0.0:{}", port);
+    let listener = tokio::net::TcpListener::bind(&bind).await.unwrap();
+
+    tracing::info!("Server: http://{}", bind);
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Serialize)]
+struct CliPrompt {
+    value: String,
+}
+
+#[derive(Serialize)]
+struct GetCliPromptResponse {
+    prompt: CliPrompt,
+}
+
+#[derive(Deserialize)]
+struct GetCliPromptRequestQuery {
+    q: String,
+}
+
+async fn get_cli_prompt(
+    Query(query): Query<GetCliPromptRequestQuery>,
+) -> Json<GetCliPromptResponse> {
+    let prompt = CliPrompt {
+        value: format!("echo \"{}\"", query.q),
+    };
+
+    Json(GetCliPromptResponse { prompt })
 }
